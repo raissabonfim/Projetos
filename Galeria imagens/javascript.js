6de85300c545cdf9b258ed4d014b0ea5a0126f21
raissabonfim <raissa230306@edu.unipar.br>
2024-05-08 20:32:31 -0300

    terceirocomiit

---
+++ Galeria imagens/javascript.js
@@ -0,0 +1,31 @@
+console.log("----A");
+const teste = 50;
+
+// LET usado para declarar variáveis que os valores podem se retribuidos
+let C = 0;
+C = 10;
+console.log(C);
+
+// o vento DOM é acionado quando todo o HTML foi completamente carregado
+document.addEventListener('DOMContentLoaded', function(){
+    alert(123)
+
+    //Tipos de Dados
+    //string: Sequencia de caracter 
+    //Number: números inteiros ou pontos flutuantes (10.4)
+    //Boolean: Retorna dois valores, true ou false
+    //object: estrutura de dados que pode armazenar múltiplos valores
+    //NULL: quando está vazio
+    //array: objeto especial ordenado
+    //function: bloco de código reutilizável
+
+    //var A = 0;
+    
+    //Const: usado para declarar uma variavel  cujo valor é permanente 
+    //const teste = 50;
+
+    // LET usado para declarar variáveis que os valores podem se retribuidos
+    //let C = 0;
+   //C = 10;
+    //console.log(C);
+}
